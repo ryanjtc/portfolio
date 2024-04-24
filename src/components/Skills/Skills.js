55ffcbdd7c5846d0edcad163578d7a01e63795a5
Ryan Cunningham <80803741+ryanjtc@users.noreply.github.com>
2024-04-24 14:09:50 -0400

    Skills Update
    Updates the Languages section.

---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Container} from "react-bootstrap";
+
+const Skills = () => {
+    return (
+        <div className={'text-black bg-light'}>
+            <section id={'skills'}>
+                <Container className={'p-5'}>
+                    <h1>Languages</h1>
+                    <p className={'lead'}>
+                        Primarily focused on the following developer tools:
+                    </p>
+                    <ul className="styled-list">
+                        <li>React JS</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>SASS</li>
+                        <li>Bootstrap</li>
+                        <hr/>
+                        <p className={'lead'}>Database Tools:</p>
+                        <li>SQL</li>
+                        <li>Firebase</li>
+                        <li>PostgreSQL</li>
+                        <hr/>
+                        <p className={'lead'}>Systems:</p>
+                        <li>MacOS</li>
+                        <li>Windows</li>
+                        <li>Linux Virtual Machines</li>
+                    </ul>
+                </Container>
+            </section>
+        </div>
+    );
+};
+
+export default Skills;
