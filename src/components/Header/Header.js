70a595748df08994a1be37b1643c9579db086af8
Ryan Cunningham <80803741+ryanjtc@users.noreply.github.com>
2024-04-24 14:24:03 -0400

    Adding social logos to the navigation.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Navbar, Nav } from "react-bootstrap";
-
+import linkedin_image from "../../Images/Linkedin/LI-In-Bug.png";
+import github_image from "../../Images/Github/github-mark.png";
 
 const Header = () => {
     return (
@@ -16,6 +17,12 @@ const Header = () => {
                             <Nav.Link href="#links">Links</Nav.Link>
                             <Nav.Link href="#contact">Contact</Nav.Link>
                         </Nav>
+                        <a href={'https://www.linkedin.com/in/ryan-cunningham-a26618143'} target="_blank" rel="noopener noreferrer">
+                            <img src={linkedin_image} alt={'LinkedIn Logo'} className="img-fluid m-2" style={{maxWidth: 35, height: "auto"}} />
+                        </a>
+                        <a href={'https://github.com/ryanjtc'} target="_blank" rel="noopener noreferrer">
+                            <img src={github_image} alt={'GitHub Logo'} className="img-fluid m-2" style={{maxWidth: 34, height: "auto"}} />
+                        </a>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
